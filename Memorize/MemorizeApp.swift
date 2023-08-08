8bbc6617cd06a9a73b5856891d15e69f46ebca32
SeanWang <seanwang0111@gmail.com>
2023-08-08 13:48:12 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 王奕翔 on 2023/8/8.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
